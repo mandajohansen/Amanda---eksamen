5fd1baa8ff93c0741d70da61e02613809bf1db3e
Jung21 <99954476+Jung21@users.noreply.github.com>
2022-04-07 14:36:23 +0200

    Jeg har lavet hele systemet til at definere rum

--- Assets/Scirpts/RoomInfo.cs
+++ Assets/Scirpts/RoomInfo.cs
@@ -2,26 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class RoomInfo : MonoBehaviour
 {
-    public acessType acess;
-    public Text text;
-    public enum acessType
+
+    public string roomNavn;
+
+    public string roomType;
+
+    public string dato≈bning;
+
+    public acessType cleareance;
+    
+    public TMP_Text textNavn;
+
+    public TMP_Text textType;
+
+    public TMP_Text textAccess;
+
+    public TMP_Text text≈bning;
+
+    public enum acessType 
     {
-        FreeAccess = 0,
-        LimitedAccess = 1,
-        NoAccess = 2,
-        OneTimeAccess = 3
+        FriAdgang = 0,
+        BegrÊnsetAdgang = 1,
+        IngenAdgang = 2
+       
 
 
     }
 
     public void Start()
     {
-        text.text = acess.ToString();
+
+        string accessText = "";
+        if (cleareance == acessType.FriAdgang)
+        {
+            accessText = "Adgang";
+
+        }
+        if (cleareance == acessType.BegrÊnsetAdgang)
+        {
+            accessText = "BegrÊnset adgang";
+
+        }
+        if (cleareance == acessType.IngenAdgang)
+        {
+            accessText = "Ingen adgang";
+
+        }
 
 
+        textAccess.text = accessText;
+
+        textNavn.text = "Lokale: " + roomNavn;
+
+        textType.text = roomType;
+
+        text≈bning.text = dato≈bning;
     }
 
 }
