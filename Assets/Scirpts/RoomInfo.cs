c85444a3b19a57c3e62cb4f493a816267b5ded79
Jung21 <99954476+Jung21@users.noreply.github.com>
2022-04-07 21:13:05 +0200

    I commented in the RoomInfo script

--- Assets/Scirpts/RoomInfo.cs
+++ Assets/Scirpts/RoomInfo.cs
@@ -6,26 +6,38 @@ using TMPro;
 
 public class RoomInfo : MonoBehaviour
 {
-
+    //This string determines the displayed room name
     public string roomNavn;
 
+    //This string determines the displayed room type
     public string roomType;
 
+    //This string determines the displayed date of entering the room  - May scrap this later
     public string datoÅbning;
 
-    public acessType cleareance;
+    // Custom accessType
+    public accessType cleareance;
     
+    // Custom Text Mesh Pro Text input
     public TMP_Text textNavn;
 
+    // Custom Text Mesh Pro Text input
     public TMP_Text textType;
 
+    // Custom Text Mesh Pro Text input
     public TMP_Text textAccess;
 
+    // Custom Text Mesh Pro Text input
     public TMP_Text textÅbning;
 
+    // Changes colour for the different access types
     public TMP_ColorGradient farve;
 
-    public enum acessType 
+    /// <summary>
+    /// Three custom string values as enumaroters to assign access types of rooms: Afgang, BegrænsetAdgang, IngenAdgang
+    /// </summary>
+
+    public enum accessType 
     {
         Adgang = 0,
         BegrænsetAdgang = 1,
@@ -35,35 +47,41 @@ public class RoomInfo : MonoBehaviour
 
     }
 
+    // The definition of data of the rooms happen in start
     public void Start()
     {
-
+        //This string holds the information of the room access data(Which type)
         string accessText = "";
-        if (cleareance == acessType.Adgang)
+
+        //These if statements are looking for which access type is chosen for the room, and changes the colour accordingly
+        if (cleareance == accessType.Adgang)
         {
             accessText = "Alle har Adgang";
             textAccess.color = Color.green;
         }
-        if (cleareance == acessType.BegrænsetAdgang)
+        if (cleareance == accessType.BegrænsetAdgang)
         {
             accessText = "Begrænset adgang";
             textAccess.color = Color.yellow;
 
         }
-        if (cleareance == acessType.IngenAdgang)
+        if (cleareance == accessType.IngenAdgang)
         {
             accessText = "Ingen adgang";
             textAccess.color = new Color(1, 0, 0);
 
         }
 
-
+        //Displays the type of access of the room
         textAccess.text = accessText;
 
+        //Displays the name of the room
         textNavn.text = "Lokale: " + roomNavn;
 
+        //Displays the type of room
         textType.text = roomType;
 
+        //Displays the date of opening the room, for now as manual writing
         textÅbning.text = datoÅbning;
 
         
