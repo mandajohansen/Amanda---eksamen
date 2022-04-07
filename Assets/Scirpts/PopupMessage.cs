f5f5447c09d90b97b76a0629ec5a1afe330d4704
mandajohansen <aljo21@student.aau.dk>
2022-04-07 13:20:02 +0200

    Popupmessage

---
+++ Assets/Scirpts/PopupMessage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupMessage : MonoBehaviour
+{
+    
+    //Her skal den refferet til en button
+    [SerializeField] Button _button1;
+    [SerializeField] Button _button2;
+    [SerializeField] Button _button3;
+    [SerializeField] Text _button1Text;
+    [SerializeField] Text _button2Text;
+    [SerializeField] Text _button3Text;
+    [SerializeField] Text _popupText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
