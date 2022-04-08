34f3a9d4d930148e693f4f9fc74b8821a4ea9f56
DavidRW00 <davi2479@gmail.com>
2022-04-08 12:57:52 +0200

    Kommentar til Spawner script

--- Assets/Pop up/Spawner.cs
+++ Assets/Pop up/Spawner.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    /// <summary>
+    /// Liste som gør man kan tilføje i inspector, da den er en public access modifier. 
+    /// public int gør det samme så man kan redigere i inspector. 
+    /// </summary>
    public GameObject[] objectsToSpawn;
-        // Liste over objekterne der kan spawne i banen
-
+  
     public int popupCounter;
 
+    
+    // bool gør at den enten kan være true eller false. Ved Reset starter den altid som false.
+    
     bool isEnabled = false;
 
+   
+    // private void start gør brug af Reset. Fra første frame benytter vi Reset til at nulstille funktionen af gameobjekterne i popupCounter. 
+    
     private void Start() {
        Reset();
     }
-    void Update() 
+
+  
+   
+    void Update()
+    // void Update bruges da den kontant skal se om den er true eller false. 
+    // Derefter laver vi en if-else statement, hvor en handling skal ske hver gang man trykker "Jump" eller "Space" 
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            if(isEnabled == false)
+
+            // if(isEnabled == false) tjekker om den er false. Hvis den er false spawner den objektet og gør den true. 
+            // Hvis den ikke er false går den ned i else og laver et Reset. 
+            if (isEnabled == false)
             {
                 SpawnObject();
                 isEnabled=true;
@@ -35,12 +52,20 @@ public class Spawner : MonoBehaviour
 
     public void Reset()
     {
+        // Hvert objekt skal kunne gøres aktivt ved at bruge "Jump","Space".
         foreach (GameObject obj in objectsToSpawn)
         {
             obj.SetActive(false);
         }
     }
-        //Dette er koden vi rent faktisk bruger når vi skal spawne objekterne
+    /// <summary>
+    /// Dette er koden vi rent faktisk bruger når vi skal spawne objekterne. 
+    /// Når vi trykker "Space" kalder vi på metoden objectsToSpawn. Den gør at vores objekter i vores counter bliver aktive. 
+    /// Da den er public kan vi vælge hvilken popupcounter der skal være aktiv i vores inspektor.
+    /// Vores første if-statement siger at hvis vi gør brug af element 0 skal den benytte lyden fra soundmanageren og metoden deri som hedder PlayAdgangSound.
+    /// Dette gælder også for de to andre else-if statements
+    /// </summary>
+
     public void SpawnObject()
     {
         objectsToSpawn[popupCounter].SetActive(true);
